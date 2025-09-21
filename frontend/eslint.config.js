45b97f84a6b289486c8c46228ec11e882241fa8c
C043 <mariofragnitoph@gmail.com>
2025-09-21 16:46:50 +0200

    Adds testing deps and directory

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,23 +1,42 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js"
+import globals from "globals"
+import reactHooks from "eslint-plugin-react-hooks"
+import reactRefresh from "eslint-plugin-react-refresh"
+import tseslint from "typescript-eslint"
+import { defineConfig, globalIgnores } from "eslint/config"
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["src/**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
+      reactHooks.configs["recommended-latest"],
+      reactRefresh.configs.vite
     ],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
+      parser: tseslint.parser,
+      parserOptions: {
+        project: ["./tsconfig.app.json"],
+        ecmaVersion: 2020,
+        sourceType: "module"
+      },
+      globals: globals.browser
+    }
   },
+
+  {
+    files: ["**/{vite,vitest}.config.ts", "**/*.config.{ts,mts,cts}"],
+    extends: [js.configs.recommended, tseslint.configs.recommended],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: ["./tsconfig.node.json"],
+        ecmaVersion: 2020,
+        sourceType: "module"
+      },
+      globals: globals.browser
+    }
+  }
 ])
