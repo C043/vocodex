62fc23b03f30abf4e3db7b0783b917655982f458
C043 <mariofragnitoph@gmail.com>
2025-10-04 02:42:17 +0200

    Adds authenticated auto redirect to home page test

--- frontend/tests/Auth.test.tsx
+++ frontend/tests/Auth.test.tsx
@@ -1,11 +1,18 @@
 import { describe, it, expect, vi, beforeEach } from "vitest"
 import { render, screen } from "./test-utils"
 import Home from "../src/pages/Home"
+import AuthPage from "../src/pages/AuthPage"
 import { configureStore } from "@reduxjs/toolkit"
 import authReducer from "../src/redux/reducer/authSlice"
 import themeModeSlice from "../src/redux/reducer/themeModeSlice"
+import { checkAuthentication, parseJwt } from "../src/utils/authUtils"
 
 const mockNavigate = vi.fn()
+
+vi.mock("../src/utils/authUtils", () => ({
+  checkAuthentication: vi.fn(), // Return false for unauthenticated
+  parseJwt: vi.fn()
+}))
 
 vi.mock("react-router-dom", async () => {
   const actual = await vi.importActual("react-router-dom")
@@ -15,12 +22,9 @@ vi.mock("react-router-dom", async () => {
   }
 })
 
-vi.mock("../src/utils/authUtils", () => ({
-  checkAuthentication: vi.fn(() => false) // Return false for unauthenticated
-}))
-
 describe("User not authenticated", () => {
   beforeEach(() => {
+    vi.mocked(checkAuthentication).mockReturnValue(false)
     global.fetch = vi.fn()
     mockNavigate.mockReset()
   })
@@ -43,5 +47,39 @@ describe("User not authenticated", () => {
     render(<Home />, { store: testStore })
 
     expect(mockNavigate).toHaveBeenCalledWith("/login")
+  })
+})
+
+describe("Authenticated user", () => {
+  beforeEach(() => {
+    vi.mocked(checkAuthentication).mockReturnValue(true)
+    vi.mocked(parseJwt).mockReturnValue({
+      iat: new Date().getMilliseconds(),
+      sub: 1,
+      username: "TestUser",
+      exp: 3
+    })
+    window.localStorage.setItem("vocodex-jwt", "fake-token")
+    global.fetch = vi.fn()
+    mockNavigate.mockReset()
+  })
+
+  it("should already be redirected to home page if jwt is present", () => {
+    const testStore = configureStore({
+      reducer: {
+        user: authReducer,
+        darkMode: themeModeSlice
+      },
+      preloadedState: {
+        user: {
+          isLoggedIn: true,
+          userId: 1,
+          username: "TestUser"
+        }
+      }
+    })
+    render(<AuthPage />, { store: testStore })
+
+    expect(mockNavigate).toHaveBeenCalledWith("/")
   })
 })
