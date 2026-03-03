eb3def1851c582d6c902a16e444316c1b47d51b5
C043 <mariofragnitoph@gmail.com>
2026-03-03 09:37:19 +0100

    WIP website upload - Added failing test for website content parsing - todo: fetch the url, parse the title and content and save it as an   entry

--- backend/tests/test_website_entries.py
+++ backend/tests/test_website_entries.py
@@ -17,4 +17,17 @@ async def test_website_import(client, entry_cleanup, auth_header):
     data = resp.json()
     entryId = data["id"]
     assert entryId
+
+    resp = await client.get(f"/entries/{entryId}", headers=headers)
+    assert resp.status_code == 200
+    data = resp.json()
+    title = data["title"]
+    content: str = data["content"]
+
+    assert title == "Example Domain"
+    assert (
+        "This domain is for use in documentation examples without needing permission. Avoid uses in operations."
+        in content
+    )
+
     entry_cleanup(entryId)
